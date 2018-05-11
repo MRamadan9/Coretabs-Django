78cb251a22490940b6342da34b1763397c0c193e
Mahmoud Ramadan <m_ramadn55@yahoo.com>
2018-05-12 00:26:38 +0200

    Make database(Product, Category)

---
+++ shop/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Product(models.Model):
+	name = models.CharField(max_length=100)	
+	price = models.DecimalField(max_digits=10, decimal_places=2)
+	stock = models.PositiveIntegerField()
+	created_at = models.DateTimeField(default=timezone.now)
+	description = models.TextField(blank=True)
+	slug = models.SlugField(unique=True, blank=True)
+	category = models.ForeignKey("Category", related_name='products', on_delete=models.CASCADE)
+
+	class Meta:
+		ordering = ('name', )
+
+
+class Category (models.Model):
+	name = models.CharField(max_length=100)
+	slug = models.SlugField(unique=True, blank=True)	
+	created_at = models.DateTimeField(default=timezone.now)
+	description = models.TextField(max_length=200)
+	
+
+	class Meta:
+		ordering = ('name', )
+
